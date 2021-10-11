ba233ec9f9ac65fc24a9916ccced6568b07438e3
Davide Corsi <davide.corsi@par-tec.it>
2021-10-11 14:32:25 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,17 @@ public class Main {
 		Persona p = new Persona("Fabio", 165);
 		Studente s = new Studente("Giacomo", 170, "ID134");
 		Persona p1 = new Studente("Francesco", 184, "ID156");
+		Studente s1 = null;
 		s.getMatricola();
 		p1.getNome();
 //		p1.getMatricola();
 		if (p1 instanceof Studente) {
-			Studente s1 = (Studente) p1; // cast
+			s1 = (Studente) p1; // cast
 			System.out.println(s1.getMatricola());
 		}
 		System.out.println(p);
 		System.out.println(s);
 		System.out.println(p1);
+		System.out.println(s1);
 	}
 }
--- src/Persona.java
+++ src/Persona.java
@@ -2,7 +2,7 @@
 public class Persona {
 	String nome;
 	int altezza;
-
+	
 	public Persona(String nome, int altezza) {
 		this.nome = nome;
 		this.altezza = altezza;
@@ -12,10 +12,18 @@ public class Persona {
 		return nome;
 	}
 
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
 	public int getAltezza() {
 		return altezza;
 	}
-	
+
+	public void setAltezza(int altezza) {
+		this.altezza = altezza;
+	}
+
 	public String toString() {
 		return "nome: " + nome + " altezza: " + altezza;
 	}
